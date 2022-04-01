96ebc778e8df9d04f335a4715b27443ca06df8eb
jasneljuillet <jasneljuillet1995@gmail.com>
2022-04-01 10:03:23 -0700

    another day

--- php_action/facturepdf.php
+++ php_action/facturepdf.php
@@ -53,15 +53,40 @@ if ($result->num_rows > 0) {
    </script>";
  }
 
+ function mergeArrays(...$arrays)
+ {
+ 
+     $length = count($arrays[0]);
+     $result = [];
+     for ($i=0;$i<$length;$i++)
+     {
+         $temp = [];
+         foreach ($arrays as $array)
+             $temp[] = $array[$i];
+ 
+         $result[] = $temp;
+     }
+ 
+     return $result;
+ 
+ }
+
+ $mg = mergeArrays($serviceName, $quantite, $prix);
+
 ?> 
 
 <?php 
   require_once('../vendor/autoload.php');
 
   $mpdf = new \Mpdf\Mpdf();
-echo $dataCreation;
+
   $html = '
+
+  <head>
+
   <style>
+
+
   table, th, td {
     border: 1px solid black;
     border-collapse: collapse;
@@ -72,29 +97,72 @@ echo $dataCreation;
   }
 
   </style>
-  <img src="../images/entete1.png" />
-  <table style="width:100%">
-  <tr>
-    <th>No Client</th>
-    <td>'.$clientId.'</td>
+  </head>
+
+  <body>
+  
+  <div style="width: 100%; text-align: center;">
+  
+    <div align="left" style="width: 50%;float: left;">
+    <img src="../images/entete1.png" />
+    
+  </div>
+
+  <div align="left" style="width: 31.5%;float: right;">
+  <table style="width: 100%; margin-top: -40px; ">
+  <tr style="background-color: gray;">
+    <th>Date</th>
+    <th>No Proforma</th>
   </tr>
   <tr>
-    <th>No Dossier</th>
+    <td>'.$dataCreation.'</td>
     <td>'.$numDossier.'</td>
   </tr>
+</table>
+   </div>
+</div>
+
+<p>
+  23 Ave Martin Luther King Mimine, Port-au-Prince Haiti<br>
+  Tel: (509) 3611-4522 / 3389-5169<br>
+  Email: despinasse@gmail.com / desinnasse@gmail.com
+</p>
+  <table style="width:46.5%">
   <tr>
-    <th>Nom Client</th>
-    <td>'.$nomClient.'</td>
+    <th style="text-align:center; background-color: gray;">Client</th>
   </tr>
   <tr>
-    <th>Service</th>
-    <td>' ;
-    foreach($serviceName as $s) {
-    
-    }'</td>
+    <td>'.$nomClient.'<br>'.$adresse.'.<br>'.$phone.'</td>
   </tr>
 </table>
+
+<br>
+
+  <table style="width:100%;">
+  <thead>
+  <tr style="background-color: gray;">
+  <th style="text-align:center;">SERVICE</th>
+  <th style="text-align:center;">UNITE</th>
+  <th style="text-align:center;">PRIX</th>
+  <th style="text-align:center;">TOTAL</th>
+  <th style="text-align:center;">MONNAIE</th>
+</tr>
+<thead>
+';
+for ($i = 0; $i < count($mg); $i++) {
+  $html .= '<tr>';
+  for($j = 0; $j < count($mg); $j++) {
+      $html .='<td>'.$mg[$i][$j].'</td>';
+  }
+ 
+  $html .= '</tr><td>'.$quantite[$i] * $prix[$i].'</td><td style="text-align:center;">USD</td>';
+}
+$html.=
+'
+</table>
+</body>
   ';
   $mpdf->WriteHTML($html);
+  //echo $quantite[0] * $prix[0];
   $mpdf->output();
 ?>
